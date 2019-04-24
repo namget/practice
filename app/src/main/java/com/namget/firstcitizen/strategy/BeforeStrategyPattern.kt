7fe72349055984fd9a11300b3612f37792047128
LeeJaeWoo <yeboykr@naver.com>
2019-04-24 21:58:57 +0900

    -스트래티지 패턴 소스 구현

--- app/src/main/java/com/namget/firstcitizen/strategy/BeforeStrategyPattern.kt
+++ app/src/main/java/com/namget/firstcitizen/strategy/BeforeStrategyPattern.kt
@@ -1,7 +1,49 @@
 package com.namget.firstcitizen.strategy
 
 abstract class BeforeDuck(){
-    fun quack() = println("꿱꿱")
+    open fun quack() = println("꿱꿱")
     fun swim() = println("수영가능합니다.")
+    open fun fly() = println("나는 날 수 있어!")
     abstract fun display()
+}
+
+class NotFlyDuck() : BeforeDuck(){
+    override fun fly() = println("나는 날지 못해!!!!!")
+    override fun display() {
+        //러버덕
+    }
+}
+
+
+
+interface Flyable{
+    fun fly()
+}
+
+interface Quackable{
+    fun quack()
+}
+
+class MyDuck() : BeforeDuck() , Flyable , Quackable{
+    override fun display() {
+        //
+    }
+
+    override fun fly() {
+        //하늘을 난다.
+    }
+
+    override fun quack() {
+        //꿱꿱
+    }
+}
+
+
+class RubberDuck() : BeforeDuck(){
+    override fun quack() {
+        println("삑삑")
+    }
+    override fun display() {
+      //러버덕
+    }
 }
