51e67d5180dd811cebd893d1d43094e6d0c17af1
leejaewoo <yeboykr@naver.com>
2019-04-29 14:10:18 +0900

    1. observer pattern 공부중

--- app/src/main/java/com/namget/firstcitizen/strategy/BeforeStrategyPattern.kt
+++ app/src/main/java/com/namget/firstcitizen/strategy/BeforeStrategyPattern.kt
@@ -1,7 +1,7 @@
 package com.namget.firstcitizen.strategy
 
 abstract class BeforeDuck(){
-    fun quack() = println("꿱꿱")
+    open fun quack() = println("꿱꿱")
     fun swim() = println("수영가능합니다.")
     open fun fly() = println("나는 날 수 있어!")
     abstract fun display()
