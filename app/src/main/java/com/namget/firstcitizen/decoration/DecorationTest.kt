810441b452a4db363b540b0fd2b2a1051b6fa3b7
leejaewoo <yeboykr@naver.com>
2019-04-29 18:10:10 +0900

    1. observer pattern study 2. decoration pattern study

---
+++ app/src/main/java/com/namget/firstcitizen/decoration/DecorationTest.kt
@@ -0,0 +1,20 @@
+package com.namget.firstcitizen.decoration
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class DecorationTest :  AppCompatActivity(){
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
+
+
+
+class Beverage{
+    lateinit var description : String
+
+
+}
