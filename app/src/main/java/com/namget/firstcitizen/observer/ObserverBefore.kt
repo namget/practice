16aa7a1ac7cd90975afd71b46fa536494be22347
LeeJaeWoo <yeboykr@naver.com>
2019-04-29 22:23:02 +0900

    -옵저버패턴 구현 완료

---
+++ app/src/main/java/com/namget/firstcitizen/observer/ObserverBefore.kt
@@ -0,0 +1,17 @@
+package com.namget.firstcitizen.observer
+
+//class WeatherData {
+//    fun measurementsChanged() {
+//        val temp: Float = getTemperature()
+//        val humidity: Float = getHumidity()
+//        val pressure: Float = getPressure()
+//        //a.displayUpdate(temp,humidity,pressure)
+//        //b.displayUpdate(temp,humidity,pressure)
+//        //c.displayUpdate(temp,humidity,pressure)
+//    }
+//}
+//    fun getTemperature() : Float = 0.0f
+//    fun getHumidity() : Float = 0.0f
+//    fun getPressure() : Float = 0.0f
+
+
--- app/src/main/java/com/namget/firstcitizen/observer/ObserverTest.kt
+++ app/src/main/java/com/namget/firstcitizen/observer/ObserverTest.kt
@@ -2,6 +2,12 @@ package com.namget.firstcitizen.observer
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import java.util.*
+
+fun measurementsChanged() {
+    // 기상값이 바뀌었을때 불러짐
+
+}
 
 
 class ObserverTest : AppCompatActivity() {
@@ -17,10 +23,9 @@ class ObserverTest : AppCompatActivity() {
         val statisticsDisplay: StatisticsDisplay = StatisticsDisplay(weatherData)
         val forecastDisplay: ForecastDisplay = ForecastDisplay(weatherData)
 
-        weatherData.setMeasurements(80f, 65f, 30.4f)
-        weatherData.setMeasurements(82f, 70f, 38.2f)
-        weatherData.setMeasurements(88f, 90f, 29.2f)
-
+        weatherData.setMeasurements(80f, 65f, 30.4f) // 데이터가 바뀌는걸 주입하기 위한 함수
+        weatherData.setMeasurements(82f, 70f, 38.2f) // 데이터가 바뀌는걸 주입하기 위한 함수
+        weatherData.setMeasurements(88f, 90f, 29.2f) // 데이터가 바뀌는걸 주입하기 위한 함수
     }
 
 }
@@ -38,25 +43,29 @@ class WeatherData(
     var pressure: Float = 0.0f
 ) : Subject {
 
-
+    //구독
     override fun registerObserver(o: Observer) {
         observers.add(o)
     }
 
+    //구독 제거
     override fun removeObserver(o: Observer) {
         observers.remove(o)
     }
 
+    //옵저버에게 데이터 변화를 알려주는부분
     override fun notifyObservers() {
         observers.forEach {
             it.update(temperature, humidity, pressure)
         }
     }
 
+    //가상 스테이션으로부터 받은 데이터를 옵저버에게 알림
     fun measurementsChanged() {
         notifyObservers()
     }
 
+    //강제적인 데이터변경을 테스트하기 위한 메솓,
     fun setMeasurements(temperature: Float, humidity: Float, pressure: Float) {
         this.temperature = temperature
         this.humidity = humidity
@@ -109,6 +118,7 @@ class StatisticsDisplay(weatherData: Subject) : Observer, DisplayElement {
         this.temperature = temp
         display()
     }
+
     override fun display() {
         println("StatisticsDisplay ${temperature} F degress and humidity ${humidity} %")
     }
@@ -163,3 +173,41 @@ class ConcreteObserver() : Observer {
     }
 }
 
+
+class WeatherDataWithObservable() : Observable() {
+    var temperature: Float = 0.0f
+    var humidity: Float = 0.0f
+    var pressure: Float = 0.0f
+
+    //가상 스테이션으로부터 받은 데이터를 옵저버에게 알림
+    fun measurementsChanged() {
+        setChanged()
+        notifyObservers()
+    }
+
+    //강제적인 데이터변경을 테스트하기 위한 메솓,
+    fun setMeasurements(temperature: Float, humidity: Float, pressure: Float) {
+        this.temperature = temperature
+        this.humidity = humidity
+        this.pressure = pressure
+        measurementsChanged()
+    }
+    //코틀린에서는 자동으로 getMethod 생성
+}
+
+class DisplayWithObservable : java.util.Observer, DisplayElement {
+    lateinit var observable: Observable
+    var temperature: Float = 0.0f
+    var humidity: Float = 0.0f
+
+    override fun update(obs: Observable?, p1: Any?) {
+        (obs as WeatherData).apply {
+            this@DisplayWithObservable.temperature = this.temperature
+            this@DisplayWithObservable.humidity = this.humidity
+            display()
+        }
+    }
+    override fun display() {
+        //println
+    }
+}
