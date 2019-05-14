0bd5ceab3c297f0dd57ceb6db2c2bec92e5eb20d
leejaewoo <yeboykr@naver.com>
2019-05-14 17:53:37 +0900

    1. factory pattern 과제

--- app/src/main/java/com/namget/firstcitizen/factory/MyAbstractFactory.kt
+++ app/src/main/java/com/namget/firstcitizen/factory/MyAbstractFactory.kt
@@ -13,41 +13,162 @@ class MyAbstractFactory : AppCompatActivity() {
 }
 
 fun main2() {
+    val KRRamenStore = KRRamenStore()
+    KRRamenStore.orderRamen(RamenType.SINRAMEN)
 
+    val JPRmenStore = JPRamenStore()
+    JPRmenStore.orderRamen(RamenType.NUGURI)
 }
 
 interface RamenIngedientFactory {
-    fun putWater()
-    fun putSauce()
-    fun putNoodle()
+    fun putWater(): Water
+    fun putSauce(): Sauce
+    fun putNoodle(): Noodle
 }
 
 interface Water
 interface Sauce
 interface Noodle
 
+class Water500 : Water
+class Water450 : Water
+class SinSauce : Sauce
+class NuguriSauce : Sauce
+class ThickNoodle : Noodle
+class NormalNoodle : Noodle
+class ThinNoodle : Noodle
 
-class KRSinRamenFactory : RamenIngedientFactory {
-    override fun putWater(): Water = println()
-    override fun putSauce(): Sauce = println()
-    override fun putNoodle(): Noodle = println()
+
+class KRSinRamenIngedientFactory : RamenIngedientFactory {
+    override fun putWater(): Water = Water450()
+    override fun putSauce(): Sauce = SinSauce()
+    override fun putNoodle(): Noodle = NormalNoodle()
 }
 
-class JPSinRamenFactory : RamenIngedientFactory {
-    override fun putWater(): Water = println()
-    override fun putSauce(): Sauce = println()
-    override fun putNoodle(): Noodle = println()
+class JPSinRamenIngedientFactory : RamenIngedientFactory {
+    override fun putWater(): Water = Water450()
+    override fun putSauce(): Sauce = SinSauce()
+    override fun putNoodle(): Noodle = ThinNoodle()
 }
 
 
-class KRNugoriFactory : RamenIngedientFactory {
-    override fun putWater(): Water = println()
-    override fun putSauce(): Sauce = println()
-    override fun putNoodle(): Noodle = println()
+class KRNugoriIngedientFactory : RamenIngedientFactory {
+    override fun putWater(): Water = Water500()
+    override fun putSauce(): Sauce = NuguriSauce()
+    override fun putNoodle(): Noodle = ThickNoodle()
 }
 
-class JPNugoriFactory : RamenIngedientFactory {
-    override fun putWater(): Water = println()
-    override fun putSauce(): Sauce = println()
-    override fun putNoodle(): Noodle = println()
+class JPNugoriIngedientFactory : RamenIngedientFactory {
+    override fun putWater(): Water = Water500()
+    override fun putSauce(): Sauce = NuguriSauce()
+    override fun putNoodle(): Noodle = ThickNoodle()
+}
+
+abstract class Ramen() {
+    var name: String? = null
+    var water: Water? = null
+    var sauce: Sauce? = null
+    var noodle: Noodle? = null
+    abstract fun prepare()
+
+    fun putNoodle() {
+        println("put Noodle ")
+    }
+
+    fun putWater() {
+        println("put water ")
+    }
+
+    open fun boil() {
+        println("boil 3 minute")
+    }
+
+    fun putSauce() {
+        println("put Sauce ")
+    }
+}
+
+class SinRamen(val ramenIngedientFactory: RamenIngedientFactory) : Ramen() {
+    override fun prepare() {
+        println("Preparing $name")
+        water = ramenIngedientFactory.putWater()
+        sauce = ramenIngedientFactory.putSauce()
+        noodle = ramenIngedientFactory.putNoodle()
+    }
+}
+
+class Nuguri(val ramenIngedientFactory: RamenIngedientFactory) : Ramen() {
+    override fun prepare() {
+        println("Preparing $name")
+        water = ramenIngedientFactory.putWater()
+        sauce = ramenIngedientFactory.putSauce()
+        noodle = ramenIngedientFactory.putNoodle()
+    }
+}
+
+
+abstract class RamenStore {
+    lateinit var ramen: Ramen
+    fun orderRamen(ramenType: RamenType) {
+        println("order Ramen")
+        ramen = createRamen(ramenType)
+        ramen.prepare()
+        ramen.putWater()
+        ramen.boil()
+        ramen.putSauce()
+        ramen.putNoodle()
+    }
+
+    abstract fun createRamen(ramenType: RamenType): Ramen
+}
+
+class KRRamenStore : RamenStore() {
+    override fun createRamen(ramenType: RamenType): Ramen {
+        var ramen: Ramen
+        var ramenIngedientFactory: RamenIngedientFactory
+        when (ramenType) {
+            RamenType.SINRAMEN -> {
+                ramenIngedientFactory = KRSinRamenIngedientFactory()
+                ramen = SinRamen(ramenIngedientFactory)
+                ramen.name = "Kr SinRamen"
+            }
+            RamenType.NUGURI -> {
+                ramenIngedientFactory = KRNugoriIngedientFactory()
+                ramen = Nuguri(ramenIngedientFactory)
+                ramen.name = "Kr Nuguri"
+            }
+            else -> {
+                throw Exception()
+            }
+        }
+        return ramen
+    }
+}
+
+class JPRamenStore : RamenStore() {
+    override fun createRamen(ramenType: RamenType): Ramen {
+        var ramen: Ramen
+        var ramenIngedientFactory: RamenIngedientFactory
+        when (ramenType) {
+            RamenType.SINRAMEN -> {
+                ramenIngedientFactory = JPSinRamenIngedientFactory()
+                ramen = SinRamen(ramenIngedientFactory)
+                ramen.name = "JP SinRamen"
+            }
+            RamenType.NUGURI -> {
+                ramenIngedientFactory = JPNugoriIngedientFactory()
+                ramen = Nuguri(ramenIngedientFactory)
+                ramen.name = "JP Nuguri"
+            }
+            else -> {
+                throw Exception()
+            }
+        }
+        return ramen
+    }
+}
+
+
+enum class RamenType {
+    NUGURI, SINRAMEN
 }
